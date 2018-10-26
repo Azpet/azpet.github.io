ef29dae6a62425e9739265f1a9662bde467725be
GBeuneker <g.beuneker@gmail.com>
2018-10-26 17:10:18 +0200

    Made the particles easily switchable

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -1,18 +1,20 @@
-$(function() {
+$(function () {
+    // Change css
+    $("header").css("background-image", "none");
+    $("header").css("background-color", "white");
+
     spawnParticle();
 });
 
-function spawnParticle()
-{
-    var particle = $("<div>", {class: "particle", style:"left:" + (Math.random()*20 + 50) + "70%;"});
+function spawnParticle() {
+    var particle = $("<div>", { class: "particle", style: "left:" + (Math.random() * 20 + 50) + "70%;" });
     particle.one("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", destroy);
     $(".header-particles").append(particle);
 
-    
+
     setTimeout(spawnParticle, Math.random() * 1500 + 50);
 }
 
-function destroy()
-{
+function destroy() {
     $(this).remove();
 }
