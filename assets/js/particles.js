d5c617dcf40d9b5de9ff0b328458ff1738def408
GEERT-DESKTOP <g.beuneker@gmail.com>
2018-10-28 09:29:52 +0100

    Made the particles more like the background image

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -11,12 +11,22 @@ $(function () {
 });
 
 function spawnParticle() {
-    var particle = $("<div>", { class: "particle", style: "left:" + (Math.random() * 20 + 50) + "70%;" });
+    var particle = $("<div>", { 
+        class: "particle", 
+        style: "left:" + (Math.random() * 20 + 50) + "70%;"});
+    particle.css('transform', 'scale(' + Math.max(0.2, Math.random()) + ')');
+    var particle_bg = $("<div>", { 
+        class: "particle-background", 
+        style: "left:" + (Math.random() * 20 + 50) + "70%;"});
+    particle_bg.css('transform', 'scale(' + Math.max(0.2, Math.random()) + ')');
+
+
     particle.one("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", destroy);
+    particle_bg.one("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", destroy);
     $(".header-particles").append(particle);
+    $(".header-particles").append(particle_bg);
 
-
-    setTimeout(spawnParticle, Math.random() * 1500 + 50);
+    setTimeout(spawnParticle, 400);
 }
 
 function destroy() {
